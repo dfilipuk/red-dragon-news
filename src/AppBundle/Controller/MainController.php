b88e75850e0d7630caf0e15376b4196d76e232c7
Alex Kozlovsci <kozlovscialecsei@mail.ru>
2017-09-07 20:04:04 +0300

    add pagination on news page

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -12,15 +12,24 @@ namespace AppBundle\Controller;
 use AppBundle\Service\NewsManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
     /**
      * @Route("/main/", name="home")
      */
-    public function indexAction(NewsManager $newsManager)
+    public function indexAction(Request $request, NewsManager $newsManager)
     {
-        $news = $newsManager->findAll();
-        return $this->render("main/index.html.twig", array('news' => $news));
+        $allNews = $newsManager->findAll();
+
+        $paginator  = $this->get('knp_paginator');
+        $newsOnPage = $paginator->paginate(
+            $allNews,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render("main/index.html.twig", array('news' => $newsOnPage));
     }
 }
