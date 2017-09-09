f91342f1efbdc333de4f44874a0ef60b0e554010
Dmitry Filipuk <kupilifds@gmail.com>
2017-09-09 12:10:42 +0300

    added security token generation

--- src/AppBundle/Service/UserManager.php
+++ src/AppBundle/Service/UserManager.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Entity\Token;
 use AppBundle\Entity\User;
 use \Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -11,6 +12,9 @@ class UserManager
     private const USER_ROLE_NAME = 'ROLE_USER';
     private const MANAGER_ROLE_NAME = 'ROLE_MANAGER';
     private const ADMIN_ROLE_NAME = 'ROLE_ADMIN';
+    private const ACCOUNT_ACTIVATION_TOKEN_TYPE = 'ACTIVATION';
+    private const PASSWORD_RESET_TOKEN_TYPE = 'PASSWORD';
+    private const SECURITY_TOKEN_LENGTH = 50;
 
     private $doctrine;
     private $encoder;
@@ -24,22 +28,24 @@ class UserManager
     public function registerNewUser(User $newUser)
     {
         if (!$this->isUserAlreadyExists($newUser)) {
-            $this->setUserInfo($newUser);
-            $this->storeUserEntity($newUser);
+            $token = $this->createSecurityToken();
+            $this->prepareEntitiesForSavingInDatabase($newUser, $token);
+            $this->saveEntitiesInDatabase($newUser, $token);
         }
     }
 
-    private function isUserAlreadyExists(User $user)
+    private function isUserAlreadyExists(User $user): bool
     {
         $repository = $this->doctrine->getManager()->getRepository(User::class);
         return $repository->findOneBy(['email' => $user->getEmail()]) !== null;
     }
 
-    private function setUserInfo(User $user)
+    private function prepareEntitiesForSavingInDatabase(User $user, Token $token)
     {
         $this->encodeUserPassword($user);
         $user->setRole(self::USER_ROLE_NAME);
-        $user->setIsActive(true);
+        $user->setIsActive(false);
+        $token->setUser($user);
     }
 
     private function encodeUserPassword(User $user)
@@ -48,10 +54,21 @@ class UserManager
         $user->setPassword($encodedPassword);
     }
 
-    private function storeUserEntity(User $user)
+    private function saveEntitiesInDatabase(User $user, TOken $token)
     {
         $manager = $this->doctrine->getManager();
         $manager->persist($user);
         $manager->flush();
+        $manager->persist($token);
+        $manager->flush();
+    }
+
+    private function createSecurityToken(): Token
+    {
+        $token = new Token();
+        $tokenValue = bin2hex(openssl_random_pseudo_bytes(self::SECURITY_TOKEN_LENGTH));
+        $token->setToken($tokenValue);
+        $token->setType(self::ACCOUNT_ACTIVATION_TOKEN_TYPE);
+        return $token;
     }
 }
